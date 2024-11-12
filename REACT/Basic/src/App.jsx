a52c7da8ca43fec45780601d2481c5eb41139a14
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-11-12 20:23:08 +0530

    Handle_Events

--- REACT/Basic/src/App.jsx
+++ REACT/Basic/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import SecondPage from "./components/SecondPage";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import NewCard from "./components/NewCard";
 function App() {
   return (
     <div>
-      <Card />
+      {/* <Card /> */}
+      <NewCard/>
 
     </div>
   );
