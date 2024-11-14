560f10c2abdc098ed995b9bf1c83283bd9da87b0
Mohit Kucheriya <kucheriyamohit@gmail.com>
2024-11-14 18:44:10 +0530

    basics

---
+++ REACT/basics/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SecondPage from "./components/SecondPage";
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import NewCard from "./components/NewCard";
+function App() {
+  return (
+    <div>
+      {/* <Card /> */}
+      <NewCard/>
+
+    </div>
+  );
+}
+
+export default App;
